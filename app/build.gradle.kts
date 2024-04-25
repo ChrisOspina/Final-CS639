149f8daf39d49eabad30b0f3311f15894f36eb9c
ShellyCS <mailstosaily@gmail.com>
2024-04-25 19:13:00 -0400

    Adding SignIn and SignUp functionality.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.plateperfect"
+    namespace = "com.example.plateperf"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.plateperfect"
+        applicationId = "com.example.plateperf"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -38,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -51,14 +53,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +71,16 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+//    implementation("androidx.core:core-ktx:1.7.0")
+//    implementation ("androidx.appcompat:appcompat:1.4.1")
+//    implementation ("com.google.android.material:material:1.5.0")
+//    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+//    implementation ("com.google.firebase:firebase-auth:21.0.3")
+//    implementation ("com.google.firebase:firebase-database:20.3.1")
+//    implementation ("com.android.support:support-annotations:28.0.0")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
