7fbde2701713590f434e823b4a5e6dcb18b93cb6
ShellyCS <mailstosaily@gmail.com>
2024-04-28 17:30:13 -0400

    Displaying meals by making an API call on card click.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,12 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation ("androidx.appcompat:appcompat:VERSION")
-    implementation ("androidx.recyclerview:recyclerview:VERSION")
-    implementation("androidx.constraintlayout:constraintlayout:VERSION")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+//    implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.picasso:picasso:2.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -75,6 +79,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
 
 
 //    implementation("androidx.core:core-ktx:1.7.0")
--- app/src/main/java/com/example/plateperf/MealActivity.kt
+++ app/src/main/java/com/example/plateperf/MealActivity.kt
@@ -1,21 +1,82 @@
 package com.example.plateperf
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.plateperf.interfaces.AppService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import layout.MealAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MealActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var newArrayList: ArrayList<recipie>
+    lateinit var imagId: Array<Int>
+    lateinit var heading: Array<String>
+    private lateinit var backButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal)
 
-        val backButton: Button = findViewById(R.id.backBtn)
+        recyclerView = findViewById(R.id.recyclerView)
+        backButton = findViewById(R.id.backBtn)
+
+
+
         backButton.setOnClickListener {
             onBackPressed()
         }
+
+        // Initialize Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.spoonacular.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // Create service instance
+        val service = retrofit.create(AppService::class.java)
+
+        // Make API call and enqueue it to execute asynchronously
+        val call = service.getRecipes("1b2c5dfeedce452ea254ea6b66ba41dd")
+
+
+        call.enqueue(object : retrofit2.Callback<recipie> {
+            override fun onResponse(call: Call<recipie>, response: retrofit2.Response<recipie>) {
+                if (response.isSuccessful) {
+                    val recipeResponse = response.body()
+                    if (recipeResponse != null) {
+                        // Handle successful response
+                        val meals = recipeResponse.results
+                        val adapter = MealAdapter(meals)
+                        recyclerView.adapter = adapter
+                        Log.d("API Response", meals.toString())
+                        // Update UI with the list of meals (see next step)
+                    } else {
+                        // Handle empty response
+                        Log.d("API Response", "No recipes received")
+                    }
+                } else {
+                    // Handle API request failure
+                    Log.d("API Response", "Failed to fetch recipes: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<recipie>, t: Throwable) {
+                // Handle network errors
+            }
+        })
+        }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
-}
