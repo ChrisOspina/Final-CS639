96db3245ee12017aa78d3b8238c8a66da2d23484
ShellyCS <mailstosaily@gmail.com>
2024-04-28 00:23:21 -0400

    Changes in main activity.

--- app/src/main/java/com/example/plateperf/MainActivity.kt
+++ app/src/main/java/com/example/plateperf/MainActivity.kt
@@ -1,34 +1,34 @@
 package com.example.plateperf
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.util.Log
-//import android.widget.Toast
-//import androidx.activity.ComponentActivity
-//import com.example.plateperf.databinding.ActivityMainBinding
-//import com.example.plateperf.databinding.ActivitySignInBinding
-//import com.google.firebase.auth.FirebaseAuth
-//
-//class MainActivity : ComponentActivity() {
-//    private lateinit var binding: ActivityMainBinding
-//    private lateinit var firebaseAuth: FirebaseAuth
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        firebaseAuth = FirebaseAuth.getInstance()
-//
-//        // Sign out button click listener
-//        binding.signOutButton.setOnClickListener {
-//            Log.d("MainActivity", "Sign out button clicked") // Log message
-//            Toast.makeText(this, getString(R.string.sign_out_button_clicked), Toast.LENGTH_SHORT).show() // Toast message
-//            firebaseAuth.signOut()
-//            // Redirect to the sign-in activity after signing out
-//            val intent = Intent(this, SignInActivity::class.java)
-//            startActivity(intent)
-//            finish() // Close the MainActivity
-//        }
-//    }
-//}
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.example.plateperf.databinding.ActivityMainBinding
+import com.example.plateperf.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Sign out button click listener
+        binding.signOutButton.setOnClickListener {
+            Log.d("MainActivity", "Sign out button clicked") // Log message
+            Toast.makeText(this, getString(R.string.sign_out_button_clicked), Toast.LENGTH_SHORT).show() // Toast message
+            firebaseAuth.signOut()
+            // Redirect to the sign-in activity after signing out
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish() // Close the MainActivity
+        }
+    }
+}
