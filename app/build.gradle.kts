5c69d0929119ee5811a295380017d6fc7873b5a0
ChrisOspina <ChrisOspina99@gmail.com>
2024-04-28 21:27:21 -0400

    added birthday as an attribute to profile and gave each text field ids

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -81,17 +82,4 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-
-
-
-//    implementation("androidx.core:core-ktx:1.7.0")
-//    implementation ("androidx.appcompat:appcompat:1.4.1")
-//    implementation ("com.google.android.material:material:1.5.0")
-//    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
-//    implementation ("com.google.firebase:firebase-auth:21.0.3")
-//    implementation ("com.google.firebase:firebase-database:20.3.1")
-//    implementation ("com.android.support:support-annotations:28.0.0")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
